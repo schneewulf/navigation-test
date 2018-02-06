3183b3d23d62a111a11890ae54b222797f1fab5e
Simon Sturmer <sstur@me.com>
2018-02-06 11:20:38 +0700

    Move source code to /src

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// @flow
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+type Props = {};
+
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello BootCamp 4!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
